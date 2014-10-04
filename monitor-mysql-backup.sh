f94fe6284dbe11d20d7806eb6258dfd0eeebb8a1
patrickzyolo <patrick@kappa-mm.de>
2014-10-04 16:46:03 +0200

    check in

--- monitor-mysql-backup.sh
+++ monitor-mysql-backup.sh
@@ -4,8 +4,8 @@ NOW=$(date +"%Y-%m-%d")
 
 DB_USER="monitor"
 DB_PASS="test123"
-DB_NAME="wpdata"
-DB_FILE="wpdata.$NOW.sql"
+DB_NAME="monitor"
+DB_FILE="monitor.$NOW.sql"
 
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $DB_FILE
 
