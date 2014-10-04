0c71c8b18e95ce1857d794764398fbf5d961b196
dezi <patrick@kappa-mm.de>
2014-10-04 23:43:32 +0200

    check in

--- monitor-mysql-backup.sh
+++ monitor-mysql-backup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-NOW=$(date + "%Y%m%d")
-
 DB_USER="monitor"
 DB_PASS="test123"
 DB_NAME="monitor"
-DB_FILE="monitor.$NOW.sql"
+DB_FILE="monitor.sql"
 
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $DB_FILE
 
