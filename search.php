d2c06bf95e1003101b0e832bf804717741bec16d
patrickzyolo <patrick@kappa-mm.de>
2014-10-06 15:34:50 +0200

    check in

--- search.php
+++ search.php
@@ -1,25 +1,20 @@
 <?php
 
-$link = mysql_connect('odroid-u3.local', 'monitor', 'test123');
-if (!$link) {
-    die('Verbindung schlug fehl: ' . mysql_error());
-}
-echo 'Erfolgreich verbunden';
+$search = $_GET['search'];
 
+$link = mysql_connect('odroid-u3.local', 'monitor', 'test123');
 mysql_select_db('monitor') or die('Could not select database');
 
-$query = 'SELECT * FROM data20141006';
+$query = 'SELECT * FROM news20141006';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
-echo "<table>\n";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
+while ($line = mysql_fetch_array($result)) 
+{ 
+	if (strpos($line['Headlines'], $search) !== false) 
+	{
+		echo $line['Newspaper'] . " " . $line['Headlines'] . "\n";    
+	}
 }
-echo "</table>\n";
 
 mysql_free_result($result);
 mysql_close($link);
