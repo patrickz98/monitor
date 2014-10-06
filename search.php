7b639753b54aaf4e57e2997383ab1b00ac0b86cb
patrickzyolo <patrick@kappa-mm.de>
2014-10-06 16:38:33 +0200

    check in

--- search.php
+++ search.php
@@ -1,3 +1,20 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Finder</title>
+		<link rel="icon" type="image/x-icon" href="../news.ico" />
+		<link rel="apple-touch-icon" href="../news.png"/>
+
+		<script src="../Chart.js"></script>
+		<style type="text/css">
+			a:link { text-decoration:none; color:#000000; }
+			a:visited { text-decoration:none; color:#0063b0; }
+
+		</style>
+
+	</head>
+	<body>
+
 <?php
 
 $search = $_GET['search'];
@@ -7,6 +24,8 @@ mysql_select_db('monitor') or die('Could not select database');
 
 $query = 'SELECT * FROM news20141006';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+echo "<h1>" . $search . "</h1>\n";
 
 while ($line = mysql_fetch_array($result)) 
 {
@@ -19,4 +38,5 @@ while ($line = mysql_fetch_array($result))
 mysql_free_result($result);
 mysql_close($link);
 ?>
-
+	</body>
+</html>
