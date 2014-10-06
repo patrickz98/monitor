25efb1a3fcd85e9396108608eb892b9d1a393c4a
patrickzyolo <patrick@kappa-mm.de>
2014-10-06 15:48:37 +0200

    check in

--- search.php
+++ search.php
@@ -8,15 +8,11 @@ mysql_select_db('monitor') or die('Could not select database');
 $query = 'SELECT * FROM news20141006';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
-echo '<style type="text/css">\n'
-echo '	a:link { text-decoration:none; color:#000000; }\n'
-echo '	a:visited { text-decoration:none; color:#0063b0; }\n'
-
 while ($line = mysql_fetch_array($result)) 
 {
 	if (strpos($line['Headlines'], $search) !== false) 
 	{
-		echo "<p><a href=" . $line['link'] . ">" . $line['Headlines'] . " " . $line['Newspaper'] . "</a></p>\n";    
+		echo "<a href=" . $line['link'] . ">" . $line['Headlines'] . " " . $line['Newspaper'] . "</a>\n";    
 	}
 }
 
