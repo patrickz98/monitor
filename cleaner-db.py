38feb53dc264de77138705f2f9e8d804a339b841
patrickzyolo <patrick@kappa-mm.de>
2014-10-12 18:54:07 +0200

    check in

---
+++ cleaner-db.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import os
+import MySQLdb as mdb
+import time
+
+import blacklist
+import conf
+
+def main():
+	con = conf.con
+	bad = blacklist.bad
+
+	with con:
+		cur = con.cursor(mdb.cursors.DictCursor)
+		
+		datadb = cur.execute("SHOW TABLES")
+		rows = cur.fetchall()
+		data = [x['Tables_in_monitor'] for x in rows]
+		
+		for x in data:
+			if int(time.strftime("%Y%m%d")) - 5 > int(x[4:]):
+				print "Delt -->", x
+				cur.execute("DROP TABLE %s" % x)
+
+	
+	con.commit()
+	con.close()
+main()
