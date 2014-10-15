8c9429d00f353215c255fe8d46a52d3923255567
patrickzyolo <patrick@kappa-mm.de>
2014-10-15 19:43:54 +0200

    check in

--- blacklist.py
+++ blacklist.py
@@ -50,7 +50,7 @@ bad = ["um", "an", "von", "in", "den", "haelt", "sicher", "tritt", "essen",
        "Kommentar", "Recht", "ehrt", "spaet", "Grund", "Ein-", "Bode", 
        "ordnet", "sehe", "enger", "erstmals", "endet", "willige", 
        "ordnet", "trotz", "enger", "sehe", "ante", "Auch", "morgen",
-       "fuehren", "schwarze", "Aufs"]
+       "fuehren", "schwarze", "Aufs", "Bahn-"]
 
 exception = ["EU", "Eu", "USA", "US", "BND", "IS", "Tod", "CIA", "NSA", "ARD", "ZDF",
 	   		 "Uni", "UNI", "UNO", "CDU", "CSU", "SPD", "AFD", "Afd", "FDP", "Uno", "BMW",
