d2dbfa62d58685db5296022c93fd0cf4b9f10217
patrickzyolo <patrick@kappa-mm.de>
2015-04-24 20:47:56 +0200

    check in

--- htmlgenerator.py
+++ htmlgenerator.py
@@ -25,34 +25,34 @@ def graph(word, html):
 
 	if len(size) >= 4:
 
-		html.write('		<div style="width:100%">\n')
-		html.write('				<canvas id="%s" height="450" width="600"></canvas>\n' % word)
-		html.write('		</div>\n')
-		html.write('\n')
+		html.write('\
+		<div style="width:100%">\n\
+				<canvas id="' + word + '" height="450" width="600"></canvas>\n\
+		</div>\n\n')
 
-		html.write( '		<script>\n' )
-		html.write( '			var lineChartData%s = {\n' % word )
-		html.write( '				labels : %s,\n' % [x[-2:] + "." + x[4:-2] + "." + x[:4] for x in sort])
-		html.write( '				datasets : [\n' )
-		html.write( '					{\n' )
-		html.write( '						label: "%s",\n' % word )
-		html.write( '						fillColor : "rgba(127, 0, 127, 0.2)",\n' )
-		html.write( '						strokeColor : "rgba(127, 0, 127, 1)",\n' )
-		html.write( '						pointColor : "rgba(127, 0, 127, 1)",\n' )
-		html.write( '						pointStrokeColor : "#fff",\n' )
-		html.write( '						pointHighlightFill : "#fff",\n' )
-		html.write( '						pointHighlightStroke : "rgba(127, 0, 127, 1)",\n' )
-		html.write( '						data : %s\n' % ''.join(str([size[x] for x in sort])) )
-		html.write( '					}\n' )
-		html.write( '				]\n' )
-		html.write( '			}\n' )
-		html.write( '\n' )
-		html.write( '			window.onload = function(){\n')
-		html.write( '				var ctx%s = document.getElementById("%s").getContext("2d");\n' % (word, word) )
-		html.write( '				window.myLine%s = new Chart(ctx%s).Line(lineChartData%s, {\n' % (word, word, word) )
-		html.write( '				responsive: true, animation: true });\n' )
-		html.write( '			}\n' )
-		html.write( '		</script>\n' )
+		html.write( '\
+		<script>\n\
+			var lineChartData' + word + ' = {\n\
+				labels : ' + [x[-2:] + "." + x[4:-2] + "." + x[:4] for x in sort] + ',\n\
+				datasets : [\n\
+					{\n\
+						label: "' + word + '",\n\
+						fillColor : "rgba(127, 0, 127, 0.2)",\n\
+						strokeColor : "rgba(127, 0, 127, 1)",\n\
+						pointColor : "rgba(127, 0, 127, 1)",\n\
+						pointStrokeColor : "#fff",\n\
+						pointHighlightFill : "#fff",\n\
+						pointHighlightStroke : "rgba(127, 0, 127, 1)",\n\
+						data : ' + "".join(str([size[x] for x in sort])) + '\n\
+					}\n\
+				]\n\
+			}\n\n\
+			window.onload = function(){\n\
+			var ctx' + word + ' = document.getElementById("' + word + '").getContext("2d");\n\
+			window.myLine' + word + ' = new Chart(ctx' + word + ').Line(lineChartData' + word + ', {\n\
+			responsive: true, animation: true });\n\
+			}\n\
+		</script>\n' )
 
 
 def main(search):
@@ -71,10 +71,7 @@ def main(search):
 	cache = mysqlnews(search)
 	cache2 = sorted(mysqlnews(search), key=operator.itemgetter(2))
 
-	html.write('<!doctype html>\n')
-	html.write('<html>\n')
-	html.write('	<head>\n')
-	html.write('\n')
+	html.write('<!doctype html>\n<html>\n\t<head>\n\n\')
 
 	html.write('    	<meta http-equiv="content-type" content="text/html; charset=utf-8">\n')
 	html.write('		<title>%s</title>\n' % search)
@@ -90,20 +87,18 @@ def main(search):
 	html.write('			h2 {text-decoration:none; color:#ffffff; }\n')
 	html.write('			p {color:#ffffff; }\n')
 	html.write('			a {font-family: helvetica;}')
-	html.write('			hr {\
-			    	border: 0;\
-					height: 5px;\
-                    background-image: -webkit-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\
-                    background-image:    -moz-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\
-                    background-image:     -ms-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\
-                    background-image:      -o-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\
+
+	html.write('			hr {\n\
+			    	border: 0;\n\
+					height: 5px;\n\
+                    background-image: -webkit-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\n\
+                    background-image:    -moz-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\n\
+                    background-image:     -ms-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\n\
+                    background-image:      -o-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));\n\
                         	}\n')
 
 
-	html.write('\n')
-	html.write('		</style>\n')
-	html.write('\n')
-
+	html.write('\n		</style>\n\n')
 	html.write('	</head>\n')
 	html.write('	<body style="background: #1F2127">\n')
 	html.write('		<h1>' + search + '</h1>\n')
--- search.php
+++ search.php
@@ -7,99 +7,181 @@
 
 		<script src="../Chart.js"></script>
 		<style type="text/css">
-			a:link { text-decoration:none; color:#000000; }
-			a:visited { text-decoration:none; color:#e76e00; }
+			a { font-family: helvetica; }
+			a:link { text-decoration:none; color:#ffffff; }
+			a:visited { text-decoration:none; color:#7f007f; }
+
+      h1 {
+				font-size: 50px;
+				color: white;
+				height: 150px;
+				width: 150px;
+				border-radius: 75px;
+				background-color: #7f007f;
+				text-align: center;
+				font-weight: bold;
+				font-family: helvetica;
+				display: inline-block;
+				line-height: 150px;
+			}
+
+			h2 { font-family: helvetica; font-size: 30px; color: white; }
+			h3 { font-family: helvetica; font-size: 20px; color: white; }
+
+      div { font-family: helvetica; font-size: 20px; color: white; }
+
+			hr {
+				border: 0;
+				height: 5px;
+				background-image: -webkit-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));
+				background-image:    -moz-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));
+				background-image:     -ms-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));
+				background-image:      -o-linear-gradient(left, rgba(127,0,127,1), rgba(127,0,127,0.75), rgba(127,0,127,0));
+			}
+
 
 		</style>
 
 	</head>
-	<body>
+	<body style="background: #1F2127">
 
-	<?php
+		<?php
 
-	$search = $_GET["search"];
-	$dbname = 'monitor';
+			$search = $_GET["q"];
+			$dbname = 'monitor';
 
-	echo "\t\t<h1>" . $search . "</h1>\n";
+			date_default_timezone_set('UTC');
 
-	if (!mysql_connect('odroid-u3.local', 'monitor', 'test123')) 
-	{
-	    echo 'Konnte nicht zu mysql verbinden';
-	    exit;
-	}
+			$todate = date("Ymd");
 
-	$sql = "SHOW TABLES FROM $dbname";
-	$result = mysql_query($sql);
+			if (!mysql_connect('odroid-u3.local', 'monitor', 'test123'))
+			{
+			    echo 'Konnte nicht zu mysql verbinden';
+			    exit;
+			}
 
-	while ($row = mysql_fetch_row($result)) 
-	{
-		if (strpos($row[0], "news") !== false) 
-		{
-				if (substr($row[0], 4) == date("Ymd"))
+			echo "\t<h1>" . $search . "</h1>\n";
+
+			$tabels = mysql_query("SHOW TABLES FROM $dbname");
+			$traffic_tabels = array();
+			$traffic = array();
+
+			while($row = mysql_fetch_array($tabels))
+			{
+				if (strpos($row[0], "data") !== false)
 				{
-					echo "<h2>Heute:</h2>\n";
-					$query2 = "SELECT * FROM" . " $dbname.{$row[0]}";
-					$result2 = mysql_query($query2);
-
-					while ($line = mysql_fetch_array($result2)) 
-					{
-						if (strpos($line['Headlines'], $search) !== false) 
-						{
-							echo "\t\t<p style=\"font-size:18px;\"><a href=" 
-							. $line['link'] . " target='_blank'>" . $line['Headlines'] . " " 
-							. "(" . $line['Newspaper'] . ")"
-							. "</a></p>\n";  
-						}
-					}
-
- 					mysql_free_result($result2);
+					$traffic_tabels[] = $row[0];
 				}
-		}
-	}
+			}
 
-	$sql = "SHOW TABLES FROM $dbname";
-	$result = mysql_query($sql);
+			foreach ($traffic_tabels as $bla)
+			{
+				$tmp = mysql_query("SELECT * FROM" . " $dbname.{$bla}");
 
-	$all_rows = array();
-	while($row=mysql_fetch_array($result)) 
-	{
-		$all_rows[] = $row[0];
-	}
-
-	$all_rows = array_reverse($all_rows);
-
-	foreach ($all_rows as $news) 
-	{
-		if (strpos($news, "news") !== false) 
-		{
-				if (substr($news, 4) !== date("Ymd"))
+				while($row = mysql_fetch_array($tmp))
 				{
-					echo "<h3>" 
-					. substr($news, -2) . "." 
-					. substr($news, 8, -2) . "." 
-					. substr($news, 4, -4) 
-					. "</h3>\n";		
-				
+					if (strval($row[0]) == strval($search))
+					{
+						$traffic[(String) substr($bla, 4)] = $row[1];
+					}
+				}
+			}
+
+			if (sizeof($traffic) >= 4)
+			{
+				echo "\t<div style=\"width:100%\">\n";
+				echo "\t\t<canvas id=\"Euro\" height=\"450\" width=\"600\"></canvas>\n";
+				echo "\t</div>\n";
+
+				echo "\t<script>\n";
+				echo "\t\tvar lineChartDataEuro = {\n";
+				echo "\t\t\tlabels : [";
+
+				foreach ($traffic as $date => $size) { echo "'$date', ";}
+
+				echo "],\n";
+				echo "\t\t\tdatasets : [{\n";
+				echo "\t\t\tlabel: \"{$search}\",\n";
+				echo "\t\t\tfillColor : \"rgba(127, 0, 127, 0.2)\",\n" .
+							"\t\t\tstrokeColor : \"rgba(127, 0, 127, 1)\",\n" .
+							"\t\t\tpointColor : \"rgba(127, 0, 127, 1)\",\n" .
+							"\t\t\tpointStrokeColor : \"#fff\",\n" .
+							"\t\t\tpointHighlightFill : \"#fff\",\n" .
+							"\t\t\tpointHighlightStroke : \"rgba(127, 0, 127, 1)\",\n" .
+							"\t\t\tdata : [";
+
+							foreach ($traffic as $date => $size) { echo "'$size', ";}
+
+				echo "]\n\t\t}]\n\t}\n";
+
+				echo
+				"\t\twindow.onload = function(){\n" .
+				"\t\t\tvar ctx{$search} = document.getElementById(\"{$search}\").getContext(\"2d\");\n" .
+				"\t\t\twindow.myLine{$search} = new Chart(ctx{$search}).Line(lineChartData{$search}, {\n" .
+				"\t\t\tresponsive: true, animation: true });\n" .
+				"\t\t}\n" .
+				"\t</script>\n";
+
+			}
+
+
+			echo "\t\t<h2>Heute:</h2>\n";
+
+			$Headlines_today = mysql_query("SELECT * FROM" . " $dbname.news{$todate}");
+
+			while ($line = mysql_fetch_array($Headlines_today))
+			{
+
+				//
+				// check if q-word is in Headlines row
+				//
+
+				if (strpos($line['Headlines'], $search) !== false)
+				{
+					echo "\t\t<p style=\"font-size:20px;\"><a href="
+					. $line['link'] . " target='_blank'>" . $line['Headlines'] . " "
+					. "(" . $line['Newspaper'] . ")"
+					. "</a></p>\n";
+				}
+			}
+
+			mysql_free_result($Headlines_today);
+
+			$all_rows = array();
+
+			while($row = mysql_fetch_array($tabels))
+			{
+				if (strpos($row[0], "news") !== false && substr($row[0], 4) !== date("Ymd"))
+				{
+					$all_rows[] = $row[0];
+				}
+			}
+
+			$all_rows = array_reverse($all_rows);
+
+			foreach ($all_rows as $news)
+			{
+					echo "<h3>"
+					. substr($news, -2) . "."
+					. substr($news, 8, -2) . "."
+					. substr($news, 4, -4)
+					. "</h3>\n";
+
 					$query2 = "SELECT * FROM" . " $dbname.{$news}";
 					$result2 = mysql_query($query2);
 
-					while ($line = mysql_fetch_array($result2)) 
+					while ($line = mysql_fetch_array($result2))
 					{
-						if (strpos($line['Headlines'], $search) !== false) 
+						if (strpos($line['Headlines'], $search) !== false)
 						{
-							echo "\t\t<p style=\"font-size:18px;\"><a href=" 
-							. $line['link'] . " target='_blank'>" . $line['Headlines'] . " " 
+							echo "\t\t<p style=\"font-size:20px;\"><a href="
+							. $line['link'] . " target='_blank'>" . $line['Headlines'] . " "
 							. "(" . $line['Newspaper'] . ")"
-							. "</a></p>\n";  
+							. "</a></p>\n";
 						}
 					}
-
-					mysql_free_result($result2);
 				}
-			}
-		}
-
-		mysql_free_result($result);	
-	?>
+				mysql_free_result($tabels);
+			?>
 	</body>
 </html>
