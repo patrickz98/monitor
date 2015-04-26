3cb064715a0e3d8d5bad2b477c4002c4a70ca636
patrickzyolo <patrick@kappa-mm.de>
2015-04-26 13:51:14 +0200

    check in

--- header.py
+++ header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import time
+import sys
 
 import conf
 
-from lib import tagesspiegel 
+from lib import tagesspiegel
 from lib import welt
 from lib import zeit
 from lib import sueddeutsche
@@ -23,61 +24,86 @@ def main():
 		cur = con.cursor()
 
 		cur.execute("DROP TABLE IF EXISTS news%s" % str(time.strftime("%Y%m%d")))
-    
+
 		cur.execute("CREATE TABLE news%s(Headlines VARCHAR(300), Newspaper VARCHAR(20), link VARCHAR(400))" % \
     		str(time.strftime("%Y%m%d")))
 
+	sys.stdout.write("%-18s" % "\rtagesspiegel")
+	sys.stdout.flush()
 	try:
 		tagesspiegel.tagesspiegel()
 	except:
-		print "\033[91mSueddeutsche Offline\033[0m"
-		
+		sys.stderr.write("\033[91mTagesspiegel Offline\033[0m\n")
+
+
+	sys.stdout.write("%-18s" % "\rWelt")
+	sys.stdout.flush()
 	try:
 		welt.welt()
 	except:
-		print "\033[91mWelt Offline\033[0m"
+		sys.stderr.write("\033[91mWelt Offline\033[0m\n")
 
+
+	sys.stdout.write("%-18s" % "\rZeit Online")
+	sys.stdout.flush()
 	try:
 		zeit.zeit()
 	except:
-		print "\033[91mZeit Offline\033[0m"
+		sys.stderr.write("\033[91mZeit Online Offline\033[0m\n")
 
+
+	sys.stdout.write("%-18s" % "\rSueddeutsche")
+	sys.stdout.flush()
 	try:
 		sueddeutsche.sueddeutsche()
 	except:
-		print "\033[91mSueddeutsche Offline\033[0m"
+		sys.stderr.write("\033[91mSueddeutsche Offline\033[0m\n")
 
+	sys.stdout.write("%-18s" % "\rStern")
+	sys.stdout.flush()
 	try:
 		stern.stern()
 	except:
-		print "\033[91mStern Offline\033[0m"
+		sys.stderr.write("\033[91mStern Offline\033[0m\n")
 
+	sys.stdout.write("%-18s" % "\rSiegel Online")
+	sys.stdout.flush()
 	try:
 		spon.spon()
 	except:
-		print "\033[91mSiegel Online Offline\033[0m"
+		sys.stderr.write("\033[91mSiegel Online Offline\033[0m\n")
 
+	sys.stdout.write("%-18s" % "\rFAZ")
+	sys.stdout.flush()
 	try:
 		faz.faz()
 	except:
-		print "\033[91mFaz Offline\033[0m"
+		sys.stderr.write("\033[91mFaz Offline\033[0m\n")
 
+	sys.stdout.write("%-18s" % "\rntv")
+	sys.stdout.flush()
 	try:
 		ntv.ntv()
 	except:
-		print "\033[91mn-tv Offline\033[0m"
+		sys.stderr.write("\033[91mn-tv Offline\033[0m\n")
 
+	sys.stdout.write("%-18s" % "\rtaz.de")
+	sys.stdout.flush()
 	try:
 		taz.taz()
 	except:
-		print "\033[91mtaz.de Offline\033[0m"
-	
+		sys.stderr.write("\033[91mtaz.de Offline\033[0m\n")
+
+	sys.stdout.write("%-18s" % "\rHandelsblatt")
+	sys.stdout.flush()
 	try:
 		handelsblatt.handelsblatt()
 	except:
-	        print "\033[91mHandelsblatt Offline\033[0m"
-        
+	        sys.stderr.write("\033[91mHandelsblatt Offline\033[0m\n")
+
+	sys.stdout.write("%-18s" % "\rFocus\n")
+	sys.stdout.flush()
 	try:
 		focus.focus()
 	except:
-	        print "\033[91mFocus Offline\033[0m"
+	        sys.stderr.write("\033[91mFocus Offline\033[0m\n")
