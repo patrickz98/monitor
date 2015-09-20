ee6688b4c22053badb5ae02a13eb495864ce127b
patrickzyolo <patrick@kappa-mm.de>
2015-09-20 15:10:19 +0200

    update

--- monitor.php
+++ monitor.php
@@ -5,17 +5,17 @@
 		<title>MNMP</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link rel="icon" href="icon.png" type="image/png" />
-    <link rel="apple-touch-icon" href="icon-apple.png"/>
+        <link rel="apple-touch-icon" href="icon-apple.png"/>
 
-    <style>
-      h1 { font-size: 20px; color: white; }
-      div { font-size: 20px; color: white; }
+        <style>
+          h1 { font-size: 20px; color: white; }
+          div { font-size: 20px; color: white; }
 
-    </style>
+        </style>
 
 
 	</head>
-  <body style="background: #1F2127">
+    <body style="background: #1F2127">
 
 		<?php
 			if (!mysql_connect('odroid-u3.local', 'monitor', 'test123'))
@@ -29,8 +29,8 @@
 			mysql_query("SET NAMES 'utf8'");
 			date_default_timezone_set('UTC');
 
-			$toDate = date("Ymd");
-			$data = mysql_query("select * from data{$toDate}");
+			$today = date("Ymd");
+			$data = mysql_query("select * from data{$today}");
 			$clusters = array();
 
 			while ($dataset = mysql_fetch_row($data))
@@ -42,18 +42,18 @@
 			foreach ($clusters as $name => $size)
 			{
 //				$URLname = str_replace("\"", "", iconv("utf-8", "ascii//IGNORE", $name));
-        $URLname = urlencode(base64_encode($name));
+                $URLname = urlencode(base64_encode($name));
 
-        $rand = $size * 15;
-				$rand2 = $rand / 2;
-				$TextSize = $size * 3;
-				$LineHeight = $rand2 * 2;
+                $diameter = sqrt($size) * 60;
+				$radius = $diameter / 2;
+				$TextSize = sqrt($size) * 10;
+				$LineHeight = $radius * 2;
 
-				$url = "/search.php?q={$URLname}";
+				$destination_url = "/search.php?q={$URLname}";
 
-				$opt = "height: {$rand}px;
-						width: {$rand}px;
-						border-radius: {$rand2}px;
+				$circle_view = "height: {$diameter}px;
+						width: {$diameter}px;
+						border-radius: {$radius}px;
 						background-color: #7f007f;
 						text-align: center;
 						font-weight: bold;
@@ -64,28 +64,28 @@
 						line-height: {$LineHeight}px;
 						font-size: {$TextSize}px;";
 
-        $randNUM = $size * 3;
-        $rand2NUM = $randNUM / 2;
-        $TextSizeNUM = $size * 1.5;
+                $diameter_Num = sqrt($size) * 20;
+                $radius_Num = $diameter_Num / 2;
+                $TextSize_Num = sqrt($size) * 4;
 
 
-        $optNUM = "height: {$randNUM}px;
-    				width: {$randNUM}px;
-    				border-radius: {$rand2NUM}px;
+                $circle_view_Num = "height: {$diameter_Num}px;
+    				width: {$diameter_Num}px;
+    				border-radius: {$radius_Num}px;
     				background-color: #7f007f;
     				text-align: center;
-						font-weight: bold;
+    				font-weight: bold;
     				font-family: helvetica;
     				color: white;
     				margin: 30px;
     				display: inline-block;
-            position: absolute;
-    				line-height: {$randNUM}px;
-    				font-size: {$TextSizeNUM}px;";
+                    position: absolute;
+    				line-height: {$diameter_Num}px;
+    				font-size: {$TextSize_Num}px;";
 
 				echo "\t\t<center>
-          <a style=\"$opt\" href=\"{$url}\">{$name}</a>
-          <a style=\"$optNUM\" href=\"{$url}\">{$size}</a>
+          <a style=\"$circle_view\" href=\"{$destination_url}\">{$name}</a>
+          <a style=\"$circle_view_Num\" href=\"{$destination_url}\">{$size}</a>
           </center>\n";
 
 			}
