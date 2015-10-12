3a1dfdbe9af197ebdd39ba6c51b940b1302ab9bb
patrickzyolo <patrick@kappa-mm.de>
2015-10-12 22:43:16 +0200

    update

--- blacklist.py
+++ blacklist.py
@@ -58,8 +58,8 @@ bad = ["haelt", "sicher", "tritt", "essen", "rein", "eigene",
        "könnte", "seinen", "fünf", "zurück", "Rund", "lässt", "warte",
        "lasse", "weite", "trifft", "gleiche", "findet", "besten",
        "ließ", "trifft", "Seit", "Teil", "unser", "Main", "kennen",
-       "hör", "Dieser", "nehm", "nahm", "wenige", "geplante", "gewinnt",
-       "Stan"]
+       "hör", "Dieser", "nehm", "nahm", "wenige", "geplante",
+       "gewinnt", "Stan", "ziehen"]
 
 exception = ["EU", "Eu", "USA", "BND", "IS", "Tod", "CIA", "NSA", "ARD", "ZDF",
 	   		 "Uni", "UNI", "UNO", "CDU", "CSU", "SPD", "AFD", "Afd", "FDP", "Uno",
